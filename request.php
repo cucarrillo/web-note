a3bfcc8c22a0753f2513d5dc3234d2447b97fe21
cucarrillo <66282557+cucarrillo@users.noreply.github.com>
2020-12-04 17:48:44 -0500

    Update request.php

--- request.php
+++ request.php
@@ -1,12 +1,70 @@
 <?php
-public $dbhost     = getenv("MYSQL_SERVICE_HOST");
-public $dbusername = getenv("dbusername");
-public $dbpassword = getenv("dbpassword");
-public $dbname     = getenv("dbname");
+
+function connectDB()
+{
+    $dbhost     = getenv("MYSQL_SERVICE_HOST");
+    $dbusername = getenv("dbusername");
+    $dbpassword = getenv("dbpassword");
+    $dbname     = getenv("dbname");
+
+    $connect = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
+
+    return $connect;
+}
+
+class Note
+{
+    private $note       = null;
+    private $id         = null;
+    private $canEdit    = null;
+    private $password   = null;
+
+    function __construct($id)
+    {
+        // Connect to the database
+        $connection = connectDB();
+        
+        // Create the query search
+        $query = "SELECT * FROM notes where id = '$noteID';";
+
+        // Execute the query
+        $result = mysqli_query($connect, $note);
+
+        // Gather results
+        while($row = mysqli_fetch_assoc($result))
+        {
+            $noteMSG = $row['note'];
+
+            $this->note       = $row['note'];
+            $this->id         = $row['id'];
+            $this->canEdit    = $row['canedit'];
+            $this->password   = $row['password'];
+        }
+    }
+
+    function setMessage($note)
+    {
+        $this->note = $note;
+    }
+    
+    function submit($note)
+    {
+        
+    }
+
+    function getID()        { return $this->id; }
+    function getMessage()   { return $this->note; }
+    function canEdit()      { return $this->canEdit; }
+    function getPassword()  { return $this->password; }
+}
 
 function getNote($noteID)
 {
-
+    return new Note($noteID);
+    /*$dbhost     = getenv("MYSQL_SERVICE_HOST");
+    $dbusername = getenv("dbusername");
+    $dbpassword = getenv("dbpassword");
+    $dbname     = getenv("dbname");
 
     $connect = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
     $noteMSG = null;
@@ -26,12 +84,12 @@ function getNote($noteID)
 
     $connect->close();
 
-    return $noteMSG;
+    return $noteMSG;*/
 }
 
 if(isset($_GET['id']))
 {
-    echo getNote($_GET['id']);
+    echo getNote($_GET['id'])->getMessage();
 
 }
 
