fe7dd9669004264384a8ddd4e69e00c508ef2827
cucarrillo <66282557+cucarrillo@users.noreply.github.com>
2020-12-04 16:01:04 -0500

    Update request.php

--- request.php
+++ request.php
@@ -1,3 +1,23 @@
 <?php
 echo "PHP Enabled";
+
+$serverName = "172.25.116.125";
+$serverUsername = "webnoteusr";
+$serverPassword = "webnoteusr";
+$serverName = "webnote";
+
+$connect = mysqli_connect($serverName, $serverUsername, $serverPassword, $serverName);
+$sql = "SELECT * FROM notes;";
+$result = mysqli_query($connect, $sql);
+$resultCheck = mysqli_num_rows($result);
+
+if($resultCheck > 0)
+{
+    while($row = mysqli_fetch_assoc($result))
+    {
+        echo $row['id'];
+        echo $row['note'];
+    }
+}
+
 ?>
