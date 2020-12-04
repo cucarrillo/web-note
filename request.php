81e8255ef27b297ada8017e410bfa3ea5164b19f
cucarrillo <66282557+cucarrillo@users.noreply.github.com>
2020-12-04 16:13:47 -0500

    Update request.php

--- request.php
+++ request.php
@@ -1,25 +1,26 @@
 <?php
-echo "PHP Enabled";
 
-$serverName = $_POST["ip"];
-$serverUsername = "webnoteusr";
-$serverPassword = "webnoteusr";
-$serverName = "webnote";
-
-$connect = mysqli_connect($serverName, $serverUsername, $serverPassword, $serverName);
-$sql = "SELECT * FROM notes;";
-$result = mysqli_query($connect, $sql);
-$resultCheck = mysqli_num_rows($result);
-
-if($resultCheck > 0)
+if(isset($_POST["ip"]))
 {
-    while($row = mysqli_fetch_assoc($result))
-    {
-        echo $row['id'];
-        echo $row['note'];
-    }
-}
+    $serverName = $_POST["ip"];
+    $serverUsername = "webnoteusr";
+    $serverPassword = "webnoteusr";
+    $serverName = "webnote";
 
+    $connect = mysqli_connect($serverName, $serverUsername, $serverPassword, $serverName);
+    $sql = "SELECT * FROM notes;";
+    $result = mysqli_query($connect, $sql);
+
+    if($resultCheck > 0)
+    {
+        while($row = mysqli_fetch_assoc($result))
+        {
+            echo $row['id'];
+            echo $row['note'];
+        }
+    }
+
+}
 ?>
 
 <form action="request.php" method="get">
