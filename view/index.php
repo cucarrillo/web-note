0e9cff284e935a97cb163417d2be02ea484047d6
cucarrillo <66282557+cucarrillo@users.noreply.github.com>
2020-12-13 12:41:45 -0500

    Update index.php

--- view/index.php
+++ view/index.php
@@ -1,20 +1,21 @@
 <?php
-// Imports
+/* Imports */
 require "webnote.php";
 
-// Entry Point
+/* PHP Entry code */
 function main()
 {
+    // Check if submit has value & if the value is "Update"
     if(hasValue("submit"))
     {
         if($_POST["submit"] == "Update")
         {
-            updateNote($_POST["id"], $_POST["note"]);
+            updateNote($_POST["id"], $_POST["note"]); // update the note
         }
     }
 }
 
-// Execute main
+/* Execute main */
 main();?>
 
 <!-- Web-Note created by Cesar Ubaldo Carrillo -->
@@ -32,13 +33,23 @@ main();?>
         <div class="pageContainer">
             <form action="#" method="post" id="updateForm">
                 <div class="pageBlock"> 
-                    <h1>Create Note</h1>
+                    <h1>View Note</h1>
                 </div>
+                
                 <div class="pageBlock">
-                    <input class="textBox" type="text" name="id" placeholder="Note ID" value="<?php echo hasValue('id') ? $_POST['id'] : ''; ?>">
-                </div>
+                    <input  class="textBox" 
+                            type="text" 
+                            name="id" 
+                            placeholder="Note ID" 
+                            value="<?php /* Check if value was passed */ echo hasValue('id') ? $_POST['id'] : ''; ?>">
+                </div
+                >
                 <div class="pageBlock">
-                    <input class="textBox" type="password" name="password" placeholder="Note Password" value="<?php echo hasValue('password') ? $_POST['password'] : '' ?>">
+                    <input  class="textBox" 
+                            type="password" 
+                            name="password" 
+                            placeholder="Note Password" 
+                            value="<?php /* Check if value was passed */ echo hasValue('password') ? $_POST['password'] : '' ?>">
                 </div>
 
                 <div class="pageBlock">
@@ -48,11 +59,18 @@ main();?>
                 <br>
                 
                 <div class="pageBlock">
-                    <textarea class="textSubmission" name="note" form="updateForm" placeholder="Note Text" <?php /* Check if the note is editable */ echo loadNote($_POST['id'], $_POST['password'])[1] ? '' : 'disabled' ?>><?php /* Load the note */ echo hasValue('id') ? loadNote($_POST['id'], $_POST['password'])[0] : ''; ?></textarea>
+                    <textarea   class="textSubmission" 
+                                name="note" 
+                                form="updateForm" 
+                                placeholder="Note Text" 
+                                <?php /* Check if the note is editable */ echo loadNote($_POST['id'], $_POST['password'])[1] ? '' : 'disabled' ?>
+                                ><?php /* Load the note */ echo hasValue('id') ? loadNote($_POST['id'], $_POST['password'])[0] : ''; ?></textarea>
                 </div>
 
                 <?php /* If editable then we show the update note button */ 
-                echo loadNote($_POST['id'], $_POST['password'])[1] ? "<div class=\"pageBlock\"><input class=\"button\" type=\"submit\" name=\"submit\" value=\"Update\"></div>" : ""; ?>
+                echo loadNote($_POST['id'], $_POST['password'])[1] ? 
+                            "<div class=\"pageBlock\"><input class=\"button\" type=\"submit\" name=\"submit\" value=\"Update\"></div>" : 
+                            "<div class=\"pageBlock\"><p>This note cannot be changed.</p></div>"; ?>
             </form>
 
             <div class="pageBlock">
