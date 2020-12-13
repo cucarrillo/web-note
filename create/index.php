24e71cbc37fdf57abe52555da914693f069ec0c9
cucarrillo <66282557+cucarrillo@users.noreply.github.com>
2020-12-13 12:21:01 -0500

    updated files

--- create/index.php
+++ create/index.php
@@ -30,6 +30,10 @@ main(); ?>
     <head>
         <link rel="stylesheet" href="style.css" type="text/css">
         <title>Web-Note Creator</title>
+        <script>
+            // button function to return to main page
+            function rtMain() { window.location.href = "/"; }
+        </script>
     </head>
     <body>
         <div class="pageContainer">
@@ -52,9 +56,18 @@ main(); ?>
                 </div>
                 
                 <div class="pageBlock">
-                    <input class="button" type="submit" name="submit" value="Create Note">
+                    <input class="button" type="submit" name="submit" value="Create note">
                 </div>
             </form>
+            
+            <div class="pageBlock">
+                <input class="button" type="button" value="Return to main page" onclick="rtMain();">
+            </div>
+
+            <div class="pageBlock">
+                <h2>Created by Cesar Ubaldo Carrillo 
+                <h2>website version 1.0</h2>
+            </div>
         </div>
     </body>
 </html>
--- view/index.php
+++ view/index.php
@@ -23,6 +23,10 @@ main();?>
     <head>
         <link rel="stylesheet" href="style.css" type="text/css">
         <title>Web-Note Viewer</title>
+        <script>
+            // button function to return to main page
+            function rtMain() { window.location.href = "/"; }
+        </script>
     </head>
     <body>
         <div class="pageContainer">
@@ -50,6 +54,15 @@ main();?>
                 <?php /* If editable then we show the update note button */ 
                 echo loadNote($_POST['id'], $_POST['password'])[1] ? "<div class=\"pageBlock\"><input class=\"button\" type=\"submit\" name=\"submit\" value=\"Update\"></div>" : ""; ?>
             </form>
+
+            <div class="pageBlock">
+                <input class="button" type="button" value="Return to main page" onclick="rtMain();">
+            </div>
+
+            <div class="pageBlock">
+                <h2>Created by Cesar Ubaldo Carrillo 
+                <h2>website version 1.0</h2>
+            </div>
         </div>
     </body>
 </html>
